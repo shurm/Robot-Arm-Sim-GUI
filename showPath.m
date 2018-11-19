650fb7afd214cc42be69a9f2e3c14453b3f7db52
mas868 <mas868@scarletmail.rutgers.edu>
2018-11-18 19:44:55 -0500

    added callback to drop down

--- showPath.m
+++ showPath.m
@@ -1,21 +1,36 @@
 function pointsPlotted = showPath(jointAnglesPath)
 %DISPLAYPATHANDMAP Summary of this function goes here
 %   Detailed explanation goes here
+    global currentconf
+	global currentlyShowingPath
+	
+	currentlyShowingPath = true;
     pauseIntervalInSeconds = 0.2;
 
     pointsPlotted = zeros(size(jointAnglesPath,1),1);
     for i = 1:size(jointAnglesPath,1)
+	
+		if ~currentlyShowingPath
+			break
+		end
         angles = jointAnglesPath(i,:);
-
+        
+        currentconf = angles;
+        
         plotLynx([angles 0]);
-        pause(pauseIntervalInSeconds);
-
+        
         hold on;
         [~,T0e2] = calculateFK_sol(angles);
         eePos = T0e2(1:3,4).';
         
         pointsPlotted(i,:) = scatter3(eePos(1),eePos(2),eePos(3));
-       
+		
+		pause(pauseIntervalInSeconds);
+
     end
+    if ~currentlyShowingPath
+        pointsPlotted = pointsPlotted(1:i-1,:);
+    end
+   
 end
 
--- testing.m
+++ testing.m
@@ -1,3 +1,3 @@
 startingConfig = [0 0 0 0 0 0];
-startingConfig = startingConfig(:,1:4);
+startingConfig = startingConfig(:,1:0);
 startingConfig
