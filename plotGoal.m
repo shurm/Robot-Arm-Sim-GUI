65725cbf0ab6f0389e6e9ad8b0ea28c362645103
mas868 <mas868@scarletmail.rutgers.edu>
2018-12-21 18:46:22 -0500

    allows the user to visibly see the desired position before movement

---
+++ plotGoal.m
@@ -0,0 +1,8 @@
+function goalPoint = plotGoal(x,y,z)
+%PLOTGOAL Summary of this function goes here
+%   Detailed explanation goes here
+hold on;
+goalPoint = scatter3(x,y,z,36,'g','filled');
+		
+end
+
--- testing.m
+++
@@ -1,3 +0,0 @@
-startingConfig = [0 0 0 0 0 0];
-startingConfig = startingConfig(:,1:0);
-startingConfig
